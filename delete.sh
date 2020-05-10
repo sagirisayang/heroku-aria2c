02b9178367c2701abc851d284d278b81b60e2e77
sagirisayang <64939358+sagirisayang@users.noreply.github.com>
2020-05-10 17:27:24 +0700

    Update delete.sh
    I imitated this:
    https://github.com/MrRobotGOD/heroku-aria2c/blob/6b6cc3f93219ee83a364339258c5b99f4c2f8d79/delete.sh

--- delete.sh
+++ delete.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-filePath=$3
-relativePath=${filepath#./downloads/}
-topPath=./downloads/${relativePath%%/*} # It will be the path of folder when it has multiple files, otherwise it will be the same as file path.
+
+filePath="./$3"
+dwnldir="./downloads/"
+tmp1=${filePath#./*/}
+folderName=${tmp1%%/*}
+folderPath=${dwnldir}${folderName}
 
 LIGHT_GREEN_FONT_PREFIX="\033[1;32m"
 FONT_COLOR_SUFFIX="\033[0m"
@@ -12,10 +15,11 @@ echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Download error or stop, start deletin
 if [ $2 -eq 0 ]; then
     exit 0
 elif [ -e "${filePath}.aria2" ]; then
-    rm -vf "${filePath}.aria2" "${filePath}"
-elif [ -e "${topPath}.aria2" ]; then
-    rm -vrf "${topPath}.aria2" "${topPath}"
+    rm -rvf "${filePath}.aria2"
+    rm -rvf "${filePath}"
+elif [ -e "${folderPath}.aria2" ]; then
+    rm -rvf "${folderPath}.aria2"
+    rm -rvf "${folderPath}"
 fi
-find "${topPath}" ! -path "${topPath}" -depth -type d -empty -exec rm -vrf {} \;
 
 echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Download error or stop, start deleting files finish"
